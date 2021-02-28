17c3d22c42e8c15d171565b93ee42bcb211e0711
Nicolas Surbayrole <nsurbayrole@quarkslab.com>
2021-02-28 18:02:52 +0100

    Limite RAM usage in docker

--- whitebox_builder/docker_script/runner.py
+++ whitebox_builder/docker_script/runner.py
@@ -6,6 +6,7 @@ import os
 import secrets
 import struct
 import subprocess
+import sys
 import tempfile
 import time
 
@@ -14,6 +15,7 @@ OUT_DIR = os.environ.get("OUT_DIR", os.path.abspath(os.path.dirname(SRC_DIR) + "
 
 def build(suffix, mkey, outdir):
 
+    print("{} begin build".format(datetime.datetime.now()), file=sys.stderr)
     ts = int(datetime.datetime.now().timestamp())
     ident = struct.pack('<I', ts)
     key = sha256(mkey + ident).digest()[:16]
@@ -24,8 +26,8 @@ def build(suffix, mkey, outdir):
             "-DWB_KEY={}".format(key.hex()),
             "-DWB_ID={}".format(ident.hex()),
             "-DWB_AESENC_KEY={}".format(secrets.token_hex(16))],
-            cwd=builddir, check=True, capture_output=True)
-        subprocess.run(["ninja"], cwd=builddir, check=True, capture_output=True)
+            cwd=builddir, check=True)
+        subprocess.run(["ninja", '-j', '1'], cwd=builddir, check=True)
         with open(builddir + "/libwblib.so", "rb") as f:
             whitebox_lib = f.read()
 
@@ -35,6 +37,7 @@ def build(suffix, mkey, outdir):
 
     os.chmod(outfile, 0o644)
     os.rename(outfile, outdir + "/libwblib.so")
+    print("{} finish build".format(datetime.datetime.now()), file=sys.stderr)
 
 def run(timer, suffix, key, outdir):
 
