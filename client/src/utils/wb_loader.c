19569f24a1ea8a90fd32487ee9916f271e848ea1
Nicolas Surbayrole <nsurbayrole@quarkslab.com>
2021-06-01 17:21:58 +0200

    Add license headers

--- client/src/utils/wb_loader.c
+++ client/src/utils/wb_loader.c
@@ -1,3 +1,20 @@
+/*
+ *  Copyright 2021 Nicolas Surbayrole
+ *  Copyright 2021 Quarkslab
+ *  Copyright 2021 Association STIC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 #define _GNU_SOURCE
 #ifndef HTTP_WITH_VLC
 #include <curl/curl.h>
